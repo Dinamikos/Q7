ffc53cf3684229c124f2484f4c5e0f404b777d88
SrGitQ <jmbalanzar@gmail.com>
2022-03-27 01:15:20 -0600

    parallel and bugs fixed

--- HPC/Matrix_Multiplication_CA7/P_Column.c
+++ HPC/Matrix_Multiplication_CA7/P_Column.c
@@ -3,11 +3,10 @@
 #include <time.h>
 #include <omp.h>
 
-void print_mat(int* Matrix1, int r, int c );
-void random_matrix(int* Matrix1, int r, int c);
 
+int n;
 
-void matrix_mult(int Matrix1[n][n], int Matrix2[n][n], int result_mat[n][n], int n)
+void matrix_mult(int Matrix1[n][n], int Matrix2[n][n], int result_mat[n][n], int n, int thread_counter)
 {
 	int i, j, k;
 
@@ -19,6 +18,7 @@ void matrix_mult(int Matrix1[n][n], int Matrix2[n][n], int result_mat[n][n], int
 	}
 
 	// Multiplying first Matrix and second Matrix and storing in result_mat.
+  #pragma omp parallel for num_threads(thread_counter) private(j,k) shared(result_mat, Matrix1, Matrix2)
 	for(i = 0; i < n; ++i)	{
 		for(j = 0; j < n; ++j){
 			for(k=0; k<n; ++k){
@@ -29,41 +29,42 @@ void matrix_mult(int Matrix1[n][n], int Matrix2[n][n], int result_mat[n][n], int
 }
 
 
-void random_matrix(int* Matrix, int r, int c){
-  int j, i;
-   /* Putting 1 to 12 in the 1D array in a sequence */
-  srand(time(NULL));
-  for (int i = 0; i < r; i++){
-    for (int j=0; j< c; j++){
-      Matrix[i * c + j] = rand()%10;
-      }
+void random_matrix(int Matrix[n][n], int n){
+  int i,j;
+  srand(time(NULL)); //seed to get random num
+  for(i = 0; i < n;i++) { //rows
+    for(j = 0; j < n;j++) { //columns
+      Matrix[i][j] = rand()%10; 
+    }   
   }
 }
 
-void print_mat(int* Matrix1, int r, int c ){
-  for (int i = 0; i < r; i++) {
-        for (int j = 0; j < c; j++)
-            printf("%d ", Matrix1[i * c + j]);
-        printf("\n");
-    } 
+void print_mat (int Matrix[n][n], int n){
+  int i,j;
+  for(i = 0; i < n;i++) {
+     for(j = 0; j < n;j++) { 
+       printf("\t%d ", Matrix[i][j]); 
+     } 
+     printf("\n");
+   }
 }
-
 int main(int argc, char**argv){
   int r = 10, c = 10;
-  int* Matrix1 = malloc((r * c) * sizeof(int));
-  int* Matrix2 = malloc((r * c) * sizeof(int));
-  int* result_mat = malloc((r * c) * sizeof(int));
+  n = 10;
+  int Matrix2[n][n];
+  int Matrix1[n][n];
+  int result_mat[n][n];
   int thread_counter = strtol(argv[1],NULL,10);
 
-  random_matrix(Matrix1, r,c);
-  random_matrix(Matrix2, r, c);
+  random_matrix(Matrix1,n);
+  random_matrix(Matrix2, n);
   //random_matrix(result_mat, r, c);
 
   printf("Matrix 1:\n");
-  print_mat(Matrix1, r, c);
+  print_mat(Matrix1, n);
   printf("Matrix 2:\n");
-  print_mat(Matrix2, r, c);
+  print_mat(Matrix2, n);
   printf("Result:\n");
-  matrix_mult(Matrix1, Matrix2, result_mat, r, c, 2, thread_counter);
-  print_mat(result_mat, r, c); 
+  matrix_mult(Matrix1, Matrix2, result_mat, n, thread_counter);
+  print_mat(result_mat, n); 
 }
