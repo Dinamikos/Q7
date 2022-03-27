ee9ccfd5b4469fff32f9bbb7cb9e68fc5771c9a1
SrGitQ <jmbalanzar@gmail.com>
2022-03-27 03:01:10 -0600

    functions created

--- HPC/Matrix_Multiplication_CA7/dejenme_solo/mtxmop.h
+++ HPC/Matrix_Multiplication_CA7/dejenme_solo/mtxmop.h
@@ -9,11 +9,38 @@ based in different strategies below sequential and parallel application,
 sequential/parallel function nomeclature:
     S/P_methodMM/VMM
 */
-void S_orgMM();
+void S_orgMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c);
 
-void P_rowMM();
-void P_colMM();
-void P_blockMM();
+void P_rowMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c, int thread_counter);
+void P_colMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c, int thread_counter);
+void P_blockMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c, int bsize, int thread_counter);
 
-void S_triangularVMM();
-void P_triangularVMM();
+void S_triangularVMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c);
+void P_triangularVMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c, int thread_counter);
+
+
+
+
+void S_orgMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c){
+
+}
+
+void P_rowMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c, int thread_counter){
+
+}
+
+void P_colMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c, int thread_counter){
+
+}
+
+void P_blockMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c, int bsize, int thread_counter){
+
+}
+
+void S_triangularVMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c){
+
+}
+
+void P_triangularVMM(int* matrix_A, int* matrix_B, int* matrix_C, int r, int c, int thread_counter){
+
+}
